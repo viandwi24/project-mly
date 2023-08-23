384df7d39204e4731f4b69688ef10972ef813adb
viandwi24 <viandwicyber@gmail.com>
2023-08-24 04:12:31 +0700

    PLEASE

---
+++ app.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="w-full min-h-screen text-gray-100 bg-gray-950">
+    <canvas id="canvas" />
+    <div class="bg-gray-950 top-0 left-0 fixed z-40 w-full min-h-screen flex text-center justify-center items-center">
+      <div id="result" class="flex-1 font-mono text-[7px] leading-[5px]" />
+    </div>
+    <div
+      class="fixed z-50 left-0 px-10 text-center flex justify-center items-center w-full flex-col"
+      :style="{
+        top: '50%',
+        transform: 'translateY(-50%)'
+      }"
+    >
+      <div id="subtitle-top" class="transition-all duration-300 bg-gray-900/60 py-2 px-4 rounded-lg text-2xl"></div>
+      <div id="subtitle-bottom" class="transition-all duration-300 bg-gray-900/60 py-2 px-4 rounded-lg text-xs mt-4"></div>
+    </div>
+    <div class="fixed bottom-0 left-0 z-50 bg-gray-950/80 rounded-tr-xl">
+      <button id="btn-video-pause" class="text-xs px-4 py-2">pause</button>
+    </div>
+    <div v-if="!isRunning" class="text-gray-200 z-50 bg-gray-950 w-full min-h-screen top-0 left-0 fixed flex flex-col justify-center items-center">
+      <div class="text-4xl pb-16 flex gap-8 items-center">
+        <span>PROJECT MLY</span>
+        <div class="heart" :style="{ '--width': '20px', '--height': '20px' }" />
+      </div>
+      <button @click="start" class="flex gap-2 items-center">
+        <span>CLICK TO START.</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const isRunning = ref(false)
+const beloved = new ProjectMly()
+
+
+const start = () => {
+  beloved.run(
+    document.getElementById('canvas'),
+    document.getElementById('result'),
+    document.getElementById('subtitle-top'),
+    document.getElementById('subtitle-bottom'),
+    document.getElementById('btn-video-pause')
+  )
+  isRunning.value = true
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+* {
+  font-family: 'Press Start 2P', cursive;
+}
+
+// init root vars
+:root {
+  --width: 100px;
+  --height: 100px;
+}
+
+.heart {
+  width: var(--width);
+  height: var(--height);
+  position: relative;
+  background-color: red;
+  transform: rotate(-45deg);
+  margin: calc(var(--height) / 2) auto;
+}
+
+.heart::before,
+.heart::after {
+  content: "";
+  width: var(--width);
+  height: var(--height);
+  background-color: red;
+  border-radius: var(--width);
+  position: absolute;
+}
+
+.heart::before {
+  left: 0;
+  top: calc(-1 * var(--height) / 2);
+}
+
+.heart::after {
+  left: calc(var(--width) / 2);
+  top: 0;
+}
+</style>
---
+++ utils/mly.ts
@@ -0,0 +1,630 @@
+const srt1 = `
+1
+00:00:36,900 --> 00:00:38,530
+is it fate?
+
+2
+00:00:40,400 --> 00:00:44,300
+if it's not easy, it must not be
+
+3
+00:00:45,200 --> 00:00:47,000
+At that age
+
+4
+00:00:48,850 --> 00:00:52,460
+I cherished the flowers beneath my feet
+
+5
+00:00:53,260 --> 00:00:57,740
+But then, something must have changed in me,
+
+6
+00:00:57,800 --> 00:01:01,140
+I used to feel so light
+
+7
+00:01:01,680 --> 00:01:03,840
+Now I'll try
+
+8
+00:01:05,170 --> 00:01:08,330
+I just want time
+
+9
+00:01:11,110 --> 00:01:17,270
+Wait again, I will be much better then
+
+10
+00:01:17,270 --> 00:01:20,900
+Holding on, I said:
+
+11
+00:01:21,360 --> 00:01:27,080
+I will be much better then
+
+12
+00:01:28,880 --> 00:01:32,750
+Look at the sky, I'm still here
+
+13
+00:01:32,940 --> 00:01:36,930
+I'll be alive next year
+
+14
+00:01:37,120 --> 00:01:41,070
+I can make something good, oh
+
+15
+00:01:42,010 --> 00:01:45,180
+Something good
+
+16
+00:01:45,180 --> 00:01:49,010
+Look at the sky, I'm still here
+
+17
+00:01:49,510 --> 00:01:53,230
+I'll be alive next year
+
+18
+00:01:53,990 --> 00:01:58,630
+I can make something good, oh
+
+19
+00:01:58,790 --> 00:02:01,570
+Something good
+
+20
+00:02:02,440 --> 00:02:04,110
+Are you close? 
+
+21
+00:02:06,170 --> 00:02:09,160
+Shouldn't it come to you naturally?
+
+22
+00:02:10,120 --> 00:02:12,140
+And everyone knows 
+
+23
+00:02:14,220 --> 00:02:18,790
+You're losing your gift and it's plain to see
+
+24
+00:02:19,220 --> 00:02:22,820
+But then something must have changed in me
+
+25
+00:02:22,920 --> 00:02:26,380
+I don't fear it anymore
+
+26
+00:02:27,610 --> 00:02:29,000
+Now I'm sure
+
+27
+00:02:31,200 --> 00:02:33,720
+I'm sure
+
+28
+00:02:35,400 --> 00:02:39,220
+Look at the sky, I'm still here
+
+29
+00:02:39,700 --> 00:02:43,620
+I'll be alive next year
+
+30
+00:02:43,900 --> 00:02:48,420
+I can make something good, oh
+
+31
+00:02:48,800 --> 00:02:51,120
+Something good
+
+32
+00:02:52,200 --> 00:02:56,120
+Look at the sky, I'm still here
+
+33
+00:02:56,500 --> 00:03:00,120
+I'll be alive next year
+
+34
+00:03:00,500 --> 00:03:05,420
+I can make something good, oh
+
+35
+00:03:05,600 --> 00:03:09,220
+Something good
+
+36
+00:03:26,600 --> 00:03:32,920
+Wait again, I will be much better then
+
+37
+00:03:34,800 --> 00:03:42,620
+And suddenly, I've restored your faith in me
+
+38
+00:03:44,400 --> 00:03:49,320
+Look at the sky
+
+39
+00:04:01,200 --> 00:04:04,920
+Look at the sky, I'm still here
+
+40
+00:04:05,100 --> 00:04:09,020
+I'll be alive next year
+
+41
+00:04:09,400 --> 00:04:14,020
+I can make something good, oh
+
+42
+00:04:14,400 --> 00:04:17,320
+Something good
+
+43
+00:04:34,200 --> 00:04:37,820
+Look at the sky, I'm still here
+
+44
+00:04:38,400 --> 00:04:40,900
+I'll be alive next year
+
+45
+00:04:40,900 --> 00:04:47,320
+I can make something good
+
+46
+00:04:47,700 --> 00:04:50,420
+Something good
+
+47
+00:04:51,100 --> 00:04:54,820
+Look at the sky, I'm still here
+
+48
+00:04:55,500 --> 00:04:58,820
+I'll be alive next year
+
+49
+00:04:59,100 --> 00:05:03,220
+I can make something good
+
+50
+00:05:04,200 --> 00:05:07,220
+Something good
+`
+const srt2 = `
+1
+00:00:36,900 --> 00:00:38,530
+Apakah ini takdir?
+
+2
+00:00:40,400 --> 00:00:44,300
+Jika ini tidak mudah, berarti bukan
+
+3
+00:00:45,200 --> 00:00:47,000
+Di usia itu
+
+4
+00:00:48,850 --> 00:00:52,460
+Aku menyayangi bunga-bunga di bawah kakiku
+
+5
+00:00:53,260 --> 00:00:57,740
+Namun kemudian pasti ada sesuatu yang berubah dalam diriku
+
+6
+00:00:57,800 --> 00:01:01,140
+Dulu aku merasa begitu ringan
+
+7
+00:01:01,680 --> 00:01:03,840
+Kini aku akan mencoba
+
+8
+00:01:05,170 --> 00:01:08,330
+Aku hanya ingin waktu
+
+9
+00:01:11,110 --> 00:01:17,270
+Tunggu lagi
+Aku akan jauh lebih baik nanti
+
+10
+00:01:17,270 --> 00:01:20,900
+Sambil bertahan, aku berkata
+
+11
+00:01:21,360 --> 00:01:27,080
+“Aku akan jauh lebih baik nanti”
+
+12
+00:01:28,880 --> 00:01:32,750
+Lihatlah ke langit, aku masih di sini
+
+13
+00:01:32,940 --> 00:01:36,930
+Aku akan hidup tahun depan
+
+14
+00:01:37,120 --> 00:01:41,070
+Aku bisa membuat sesuatu yang baik, oh
+
+15
+00:01:42,010 --> 00:01:45,180
+Sesuatu yang baik
+
+16
+00:01:45,180 --> 00:01:49,010
+Lihatlah ke langit, aku masih di sini
+
+17
+00:01:49,510 --> 00:01:53,230
+Aku akan hidup tahun depan
+
+18
+00:01:53,990 --> 00:01:58,630
+Aku bisa membuat sesuatu yang baik, oh
+
+19
+00:01:58,790 --> 00:02:01,570
+Sesuatu yang baik
+
+20
+00:02:02,440 --> 00:02:04,110
+Apakah kamu dekat?
+
+21
+00:02:06,170 --> 00:02:09,160
+Bukankah seharusnya itu terjadi secara alami?
+
+22
+00:02:10,120 --> 00:02:12,140
+Dan semua orang tahu
+
+23
+00:02:14,220 --> 00:02:18,790
+Kamu kehilangan bakatmu, dan itu tampak jelas
+
+24
+00:02:19,220 --> 00:02:22,820
+Namun kemudian pasti ada sesuatu yang berubah dalam diriku 
+
+25
+00:02:22,920 --> 00:02:26,380
+Aku tidak takut kepadanya lagi
+
+26
+00:02:27,610 --> 00:02:29,000
+Kini aku yakin
+
+27
+00:02:31,200 --> 00:02:33,720
+Aku yakin
+
+28
+00:02:35,400 --> 00:02:39,220
+Lihatlah ke langit, aku masih di sini
+
+29
+00:02:39,700 --> 00:02:43,620
+Aku akan hidup tahun depan
+
+30
+00:02:43,900 --> 00:02:48,420
+Aku bisa membuat sesuatu yang baik, oh
+
+31
+00:02:48,800 --> 00:02:51,120
+Sesuatu yang baik
+
+32
+00:02:52,200 --> 00:02:56,120
+Lihatlah ke langit, aku masih di sini
+
+33
+00:02:56,500 --> 00:03:00,120
+Aku akan hidup tahun depan
+
+34
+00:03:00,500 --> 00:03:05,420
+Aku masih membuat sesuatu yang baik, oh
+
+35
+00:03:05,600 --> 00:03:09,220
+Sesuatu yang baik
+
+36
+00:03:26,600 --> 00:03:32,920
+Tunggu lagi, aku akan jauh lebih baik nanti
+
+37
+00:03:34,800 --> 00:03:42,620
+Dan tiba-tiba aku memulihkan keyakinanmu padaku
+
+38
+00:03:44,400 --> 00:03:49,320
+Lihatlah ke langit
+
+39
+00:04:01,200 --> 00:04:04,920
+Lihatlah ke langit, aku masih di sini
+
+40
+00:04:05,100 --> 00:04:09,020
+Aku akan hidup tahun depan
+
+41
+00:04:09,400 --> 00:04:14,020
+Aku bisa membuat sesuatu yang baik, oh
+
+42
+00:04:14,400 --> 00:04:17,320
+Sesuatu yang baik
+
+43
+00:04:34,200 --> 00:04:37,820
+Lihatlah ke langit, aku masih di sini
+
+44
+00:04:38,400 --> 00:04:40,900
+Aku akan hidup tahun depan
+
+45
+00:04:40,900 --> 00:04:47,320
+Aku bisa membuat sesuatu yang baik, oh
+
+46
+00:04:47,700 --> 00:04:50,420
+Sesuatu yang baik
+
+47
+00:04:51,100 --> 00:04:54,820
+Lihatlah ke langit, aku masih di sini
+
+48
+00:04:55,500 --> 00:04:58,820
+Aku akan hidup tahun depan
+
+49
+00:04:59,100 --> 00:05:03,220
+Aku bisa membuat sesuatu yang baik
+
+50
+00:05:04,200 --> 00:05:07,220
+Sesuatu yang baik
+`
+
+// convert srt to object
+const srtReaderToObject = (srt: string, offsetSecond = 0) => {
+  const lines = srt.split('\n')
+  const result = []
+  let index = 0
+  let second = 0
+  let text = ''
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    if (line === '') {
+      continue
+    }
+    if (line.match(/^\d+$/)) {
+      index = parseInt(line)
+    } else if (line.match(/^\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d$/)) {
+      const times = line.split(' --> ')
+      const time1 = times[0]
+      const time2 = times[1]
+      const time1s = time1.split(':')
+      const time2s = time2.split(':')
+      const time1h = parseInt(time1s[0])
+      const time1m = parseInt(time1s[1])
+      const time1s2 = time1s[2].split(',')
+      const time1s2s = parseInt(time1s2[0])
+      const time1s2ms = parseInt(time1s2[1])
+      const time2h = parseInt(time2s[0])
+      const time2m = parseInt(time2s[1])
+      const time2s2 = time2s[2].split(',')
+      const time2s2s = parseInt(time2s2[0])
+      const time2s2ms = parseInt(time2s2[1])
+      const time1ms = time1h * 60 * 60 * 1000 + time1m * 60 * 1000 + time1s2s * 1000 + time1s2ms
+      const time2ms = time2h * 60 * 60 * 1000 + time2m * 60 * 1000 + time2s2s * 1000 + time2s2ms
+      second = (time1ms / 1000) + offsetSecond
+    } else {
+      text = line
+      result.push({ index, second, text })
+    }
+  }
+  return result
+}
+
+export class ProjectMly {
+  canvas!: HTMLCanvasElement
+  video!: HTMLVideoElement
+  result!: HTMLDivElement
+  subTop!: HTMLDivElement
+  subBottom!: HTMLDivElement
+  btnVideoPause!: HTMLButtonElement
+
+  constructor() {
+    console.log("ProjectMly constructor");
+  }
+
+  run(
+    canvas: HTMLCanvasElement,
+    result: HTMLDivElement,
+    subTop: HTMLDivElement,
+    subBottom: HTMLDivElement,
+    btnVideoPause: HTMLButtonElement,
+  ) {
+    this.canvas = canvas
+    this.result = result
+    this.subTop = subTop
+    this.subBottom = subBottom
+    this.btnVideoPause = btnVideoPause
+
+    console.log("ProjectMly run", canvas);
+
+    // prepare first
+    this.prepareAssets()
+
+    // start
+    this.start()
+  }
+
+  // prepare
+  prepareAssets() {
+    const videoSourceUrl = '/example.mp4'
+    const video = document.createElement('video')
+    video.src = videoSourceUrl
+    video.autoplay = true
+    video.loop = true
+    video.muted = false
+    video.playsInline = true
+    video.style.display = 'none'
+    video.style.width = '100%'
+    video.style.height = '100%'
+    video.style.objectFit = 'cover'
+    video.style.position = 'absolute'
+    video.style.top = '0'
+    video.style.left = '0'
+    video.style.zIndex = '-1'
+    document.body.appendChild(video)
+    this.video = video
+
+    this.canvas.style.position = 'fixed'
+    this.canvas.style.display = 'none'
+    this.canvas.style.top = '0'
+    this.canvas.style.left = '0'
+    this.canvas.style.zIndex = '1'
+    this.canvas.style.width = '100%'
+    this.canvas.style.height = '100%'
+    this.canvas.style.objectFit = 'cover'
+    this.canvas.style.objectPosition = 'center'
+    this.canvas.style.background = 'transparent'
+
+
+
+    // initial video start to 30 seconds
+    video.addEventListener('loadedmetadata', () => {
+      video.currentTime = 35
+    })
+
+    // add action button
+    this.btnVideoPause.addEventListener('click', function() {
+      if (video.paused) {
+        video.play()
+        this.innerText = 'pause'
+      } else {
+        video.pause()
+        this.innerText = 'play'
+      }
+    })
+  }
+
+  private start() {
+    // create listener video element, get image per image and draw it on canvas
+    // const updater_time = 1000 / 30 // 30 fps
+    // // dr
+
+    // draw image
+    const ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D
+
+    // convert image bit with color blue to  red
+    const updater_time = 1000 / 30 // 30 fps
+
+    // datas
+    const subtitles = {
+      'top': srtReaderToObject(srt1, 1),
+      'bottom': srtReaderToObject(srt2, 1),
+    }
+    console.log('subtitles', subtitles)
+    
+    let currentAsciiResult = ''
+    const updater = () => {
+      ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height)
+      const imageData = ctx.getImageData(0, 0, this.canvas.width, this.canvas.height)
+      const data = imageData.data
+      
+      // convert image to ascii art
+      // ascii is depend on the brightness of the pixel
+      // the brighter the pixel, the lighter the ascii character
+      // the darker the pixel, the darker the ascii character
+      // the ascii character is from the darkest to the lightest
+      // 26 ascii character, more ascii character, more detail
+      let ascii = [' ', '.', '+', '*', '%', '#', '8', '@', 'M', 'W']
+      // const ascii = [ ' ', '.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@', '$', '&', '8', 'O', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'M', 'W', 'B', 'A']
+      for (let i = 0; i < data.length; i += 4) {
+
+        // if black just skip
+        if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) {
+          continue
+        }
+
+        const brightness = (data[i] + data[i + 1] + data[i + 2]) / 3
+        // if data pixel is black, skip
+        if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) {
+          continue
+        }
+        data[i] = brightness
+        data[i + 1] = brightness
+        data[i + 2] = brightness
+        data[i + 3] = 255
+      }
+
+      // draw ascii to string data depends resolution
+      const asciiResult = []
+      for (let i = 0; i < data.length; i += 4 * this.canvas.width) {
+        const asciiLine = []
+        for (let j = 0; j < this.canvas.width; j++) {
+          const asciiIndex = Math.round(data[i + j * 4] * (ascii.length - 1) / 255)
+          const asciiChar = ascii[asciiIndex]
+          asciiLine.push(asciiChar)
+        }
+        asciiResult.push(asciiLine.join(''))
+      }
+      const asciiResultString = asciiResult.join('\n')
+      if (currentAsciiResult !== asciiResultString) {
+        currentAsciiResult = asciiResultString
+      }
+      this.result.innerText = currentAsciiResult  
+
+      // ctx.putImageData(imageData, 0, 0)
+      setTimeout(updater, updater_time)
+    }
+    updater()
+    
+
+
+    // get event listener video subtitle
+    const video = this.video
+    const subTop = this.subTop
+    const subBottom = this.subBottom
+    const updateSubtitle = () => {
+      const currentTime = video.currentTime
+      const subtitleTop = subtitles['top'].filter((subtitle) => {
+        return subtitle.second <= currentTime
+      }).pop()
+      const subtitleBottom = subtitles['bottom'].filter((subtitle) => {
+        return subtitle.second <= currentTime
+      }).pop()
+      if (subtitleTop) {
+        subTop.innerText = subtitleTop.text
+      } else {
+        subTop.innerText = ''
+      }
+      if (subtitleBottom) {
+        subBottom.innerText = subtitleBottom.text
+      } else {
+        subBottom.innerText = ''
+      }
+      setTimeout(updateSubtitle, 1000 / 30)
+    }
+    updateSubtitle()
+  }
+}
